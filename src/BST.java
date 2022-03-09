313d016e68408e59f2c4a40c4dbaeffc7817e3a2
eileen huynh <eileenhuynh1064@gmail.com>
2022-03-08 17:59:36 -0800

    update compare

--- src/BST.java
+++ src/BST.java
@@ -532,10 +532,8 @@ class sortbyName implements Comparator<Product> {
     public int compare(Product p1, Product p2) {
         if(p1.equals(p2)) {
             return 0;
-        }else if(!(p1.getName().equals(p2.getName()))) {
-            return name.compareTO(p2.name)
-        }
-        return -1;
+        } 
+        return p1.getName().compareTo(p2.getName());     
     }
 }
 
@@ -549,6 +547,9 @@ class sortbyType implements Comparator<Product> {
      */
     @Override
     public int compare(Product p1, Product p2) {
-        return -1;
+        if(p1.equals(p2)) {
+            return 0;
+        } 
+        return p1.getType().compareTo(p2.getType());
     }
 }
